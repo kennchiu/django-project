247608b4a7801c2438ab18a689b217d36d82d0b7
kennchiu <Kenneth.H.Chiu@gmail.com>
2022-11-26 12:16:35 -0800

    Completed lesson 6.

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from polling.models import Poll
-
-admin.site.register(Poll)
+from blogging.models import Post
 # Register your models here.
+admin.site.register(Post)
